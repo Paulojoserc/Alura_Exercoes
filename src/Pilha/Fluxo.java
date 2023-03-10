2d2b7f938a7fe4d154cb2f2524f868c9845b6b02
Paulo Costa <pjrc83@gmail.com>
2023-03-10 16:26:42 -0300

    Lançando Exceções

--- src/Pilha/Fluxo.java
+++ src/Pilha/Fluxo.java
@@ -6,43 +6,29 @@ public class Fluxo {
 		try{
 			metodo1();
 		}catch(ArithmeticException | NullPointerException ex) {
-			//getMessage trais qual é erro 
+			
 			String msg = ex.getMessage();
 			System.out.println("Exception "+ msg);
 			
-			//trais a pilha por onde o metado passou
+			
 			ex.printStackTrace();
-		}/*catch(NullPointerException ex) {
-			//getMessage trais qual é erro 
-			String msg = ex.getMessage();
-			System.out.println("NullPointerException"+ msg);
-		}
-		*/System.out.println("Fim do main");
+		}System.out.println("Fim do main");
 	}
 
 	private static void metodo1() {
 		System.out.println("Ini do metodo1");
-		//try {
+		
 			metodo2();
-		/*}catch(ArithmeticException ex) {
-			System.out.println("ArithmeticException");
-		}*/
+		
 		System.out.println("Fim do metodo1");
 	}
 
 	private static void metodo2() {
 		System.out.println("Ini do metodo2");
-		for (int i = 1; i <= 5; i++) {
-			System.out.println(i);
-		//	try {
-				int a= i /0;
-				//Conta c = null;
-				//c.deposita();
-				
-			/*}catch(ArithmeticException ex) {
-				System.out.println("ArithmeticException");
-			}*/
-		}
-		System.out.println("Fim do metodo2");
+		
+		throw new ArithmeticException("deu errado");
+		
+		 
+		//System.out.println("Fim do metodo2");
 	} 
 }
---
+++ src/Pilha/FluxoComTratamento.java
@@ -0,0 +1,48 @@
+package Pilha;
+
+public class FluxoComTratamento {
+	public static void main(String[] args) {
+		System.out.println("Ini do main");
+		try{
+			metodo1();
+		}catch(ArithmeticException | NullPointerException ex) {
+			//getMessage trais qual é erro 
+			String msg = ex.getMessage();
+			System.out.println("Exception "+ msg);
+			
+			//trais a pilha por onde o metado passou
+			ex.printStackTrace();
+		}/*catch(NullPointerException ex) {
+			//getMessage trais qual é erro 
+			String msg = ex.getMessage();
+			System.out.println("NullPointerException"+ msg);
+		}
+		*/System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println("Ini do metodo1");
+		//try {
+			metodo2();
+		/*}catch(ArithmeticException ex) {
+			System.out.println("ArithmeticException");
+		}*/
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Ini do metodo2");
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		//	try {
+				int a= i /0;
+				//Conta c = null;
+				//c.deposita();
+				
+			/*}catch(ArithmeticException ex) {
+				System.out.println("ArithmeticException");
+			}*/
+		}
+		System.out.println("Fim do metodo2");
+	} 
+}
