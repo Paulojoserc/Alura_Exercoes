99dd7d4a118572607292622ec0c476979e9e582a
Paulojoserc <pjrc83@gmail.com>
2023-03-12 23:21:08 -0300

    Checked & Unchecked Exception

---
+++ src/bytebank_herdado_conta/ContaCorrente.java
@@ -0,0 +1,18 @@
+package bytebank_herdado_conta;
+
+public class ContaCorrente extends Conta implements Tributavel {
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+	}
+
+	@Override
+	public void saca(double valor) throws SaldoInsuficienteException {
+		double valorASacar = valor + 0.2;
+		 super.saca(valorASacar);
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
+	}
+}
---
+++ src/bytebank_herdado_conta/SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package bytebank_herdado_conta;
+
+public class SaldoInsuficienteException extends Exception{
+
+	public SaldoInsuficienteException(String msg) {
+		super(msg);
+	}
+}
